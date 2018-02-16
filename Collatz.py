5f7dd794815f231471cf1d41e2469e321bce8fbc
sjakkampudi <sri30@vt.edu>
2018-02-16 15:57:44 -0500

    Add files via upload

---
+++ Collatz.py
@@ -0,0 +1,15 @@
+import random
+import sys
+
+def collatz(num):
+	if num == 1:
+		return "1"
+
+	if num % 2 == 0:
+		return ((str(num) + " -> ") + collatz(int(num/2)))
+	else:
+		return ((str(num) + " -> ") + collatz(num*3 + 1))
+
+for i in range(9):
+	print(collatz(random.randint(1,sys.maxsize)))
+	print("\n")
