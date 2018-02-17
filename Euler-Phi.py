ce1546b91aa7d4fe1c0907ed048640d00dbfcd18
sjakkampudi <sri30@vt.edu>
2018-02-16 19:38:00 -0500

    Update Euler-Phi.py

--- Euler-Phi.py
+++ Euler-Phi.py
@@ -27,7 +27,6 @@ def count_intersection(s1, num):
 
 def sum_intersections(num):
 	sum = 0
-	#factorization = list(set(prime_factorization(num)))
 
 	for i in range(1,len(factorization)+1):
 		intersections = list(itertools.combinations(factorization, i))
