72c3b344e47827c89dd61154babe2f99686eedf6
sjakkampudi <sri30@vt.edu>
2018-02-21 12:46:54 -0500

    Add files via upload

--- Euler-Phi.py
+++ Euler-Phi.py
@@ -2,6 +2,8 @@ import itertools
 
 factorization = []
 
+# returns a list of prime factors of a given 
+# number with no repeats
 def prime_factorization(num):
 	pfactorization = []
 
@@ -13,8 +15,12 @@ def prime_factorization(num):
 		if num == 1:
 			i = num
 
-	return pfactorization
+	#set() gets rid of repeated factors
+	return list(set(pfactorization))
 
+# counts the number of numbers <= num
+# such that they are divisible by 
+# the factor(s) in the given subset s1
 def count_intersection(s1, num):
 	subset = list(s1)
 	product = 1
@@ -24,10 +30,13 @@ def count_intersection(s1, num):
 
 	return num/product
 
+# sums the counts in subsets of size i
 def sum_intersections(num):
 	sum = 0
 
 	for i in range(1,len(factorization)+1):
+		# I cheated here and used a built-in function 
+		# that returns all subsets of size i given a set
 		intersections = list(itertools.combinations(factorization, i))
 		for tup in intersections:
 			if i%2:
@@ -35,15 +44,20 @@ def sum_intersections(num):
 			else:
 				sum -= count_intersection(tup, num)
 
-	return sum;
+	return sum
 
-num = int(input("Enter an integer: "))
-factorization = list(set(prime_factorization(num)))
+num = 1
+#-----------------Script-------------------
+while(num > 0):
+	num = int(input("Enter an integer (0 to quit): "))
+	factorization = prime_factorization(num)
 
-if len(factorization) == 0:
-	print(num-1)
-else:
-	print(int(num - sum_intersections(num)))
+	# prints num-1 if num is prime
+	if (num == 0):
+		pass
+	elif len(factorization) == 0:
+		print(num-1)
+	else:
+		print(int(num - sum_intersections(num)))
 
-
-
+#------------------------------------------
